851dc577ba4abe8d69257fcac958fa55854e8a5b
Francesco <f.digiovanni2411@gmail.com>
2024-07-19 23:18:02 +0200

    created encode utils

---
+++ src/main/java/nl/sudsandbuds/utilities/EncodeUtilities.java
@@ -0,0 +1,23 @@
+package nl.sudsandbuds.utilities;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class EncodeUtilities {
+    @SuppressWarnings({"unused","unchecked"})
+    public static String encodeURI(String toBeEncoded) {
+        String encoded = "";
+        try {
+            encoded = URLEncoder.encode(toBeEncoded, "UTF-8")
+                    .replaceAll("\\+", "%20")
+                    .replaceAll("\\%21", "!")
+                    .replaceAll("\\%27", "'")
+                    .replaceAll("\\%28", "(")
+                    .replaceAll("\\%29", ")")
+                    .replaceAll("\\%7E", "~");
+        } catch (UnsupportedEncodingException e) {
+            encoded = toBeEncoded;
+        }
+        return encoded;
+    }
+}
